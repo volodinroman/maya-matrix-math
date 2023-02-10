ab97d599a223c01b919684160a2bc3c0f7fb5f59
Roman Volodin <volodinroman1988@gmail.com>
2023-02-10 02:04:41 -0800

    Update match_objects.py

--- src/match_objects.py
+++ src/match_objects.py
@@ -10,7 +10,7 @@ cube = cmds.polyCube()[0]
 cone = cmds.polyCone()[0]
 cmds.xform(cube, t=[3,5,6], ro=[13,30,50], ws=1)
 
-# get worldMatrix of the moved object
+# get worldMatrix of the 1st object
 selection_list = om.MSelectionList()
 selection_list.add(cube)
 dag_path = selection_list.getDagPath(0) # get object DagPath

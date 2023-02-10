688217884426f39f1fec7936fdfa11250d4ba285
Roman Volodin <volodinroman1988@gmail.com>
2023-02-10 02:10:38 -0800

    Update match_objects.py

--- src/match_objects.py
+++ src/match_objects.py
@@ -11,18 +11,18 @@ cone = cmds.polyCone()[0]
 cmds.xform(cube, t=[3,5,6], ro=[13,30,50], ws=1)
 
 # get worldMatrix of the 1st object
-selection_list = om.MSelectionList()
+selection_list = OpenMaya.MSelectionList()
 selection_list.add(cube)
 dag_path = selection_list.getDagPath(0) # get object DagPath
 world_matrix = dag_path.inclusiveMatrix() # inclusive matrix includes object transformations including his parents
 
 # convert world_matrx (MMatrix) into transformation matrix (MTransformationMatrix)
-transform_matrix = om.MTransformationMatrix(world_matrix)
+transform_matrix = OpenMaya.MTransformationMatrix(world_matrix)
 
 # wrap the 2nd object with MFnTransform class
 selection_list.add(cone)
 dag_path_2 = selection_list.getDagPath(1)
-fn_transform = om.MFnTransform(dag_path_2)
+fn_transform = OpenMaya.MFnTransform(dag_path_2)
 
 # MFnTransform class allows us to apply transformation matrix to an object
 fn_transform.setTransformation(transform_matrix)

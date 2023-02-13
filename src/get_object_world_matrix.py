c71d648ee491be1b0bce13cedad59cba5e657ef7
Roman Volodin <volodinroman1988@gmail.com>
2023-02-12 22:45:37 -0800

    Update get_object_world_matrix.py

--- src/get_object_world_matrix.py
+++ src/get_object_world_matrix.py
@@ -8,6 +8,8 @@ import maya.api.OpenMaya as OpenMaya
 cube = cmds.polyCube()[0]
 cmds.xform(cube, t=[1,2,3], ro=[10,15,32], ws=1)
 
+# OpenMaya way ----------------------------
+
 # get a dag path of the cube
 selection_list = om.MSelectionList()
 selection_list.add(cube)
@@ -16,4 +18,8 @@ dag_path = selection_list.getDagPath(0)
 # get world matrix
 world_matrix = dag_path.inclusiveMatrix() # gives us pCube1 world matrix
 
-print(world_matrix)
+
+# maya cmds way ---------------------------
+
+import maya.cmds as cmds
+world_matrix = cmds.xform('pCube1', q=True, worldSpace=True, matrix=True)
